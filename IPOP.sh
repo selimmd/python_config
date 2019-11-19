a1eed0765aeb730fb23c748105f756941f0f59db
Md Selim <10825767+selim-iitdu@users.noreply.github.com>
2019-11-19 17:30:27 -0500

    Update IPOP.sh

--- IPOP.sh
+++ IPOP.sh
@@ -9,3 +9,21 @@ pip install --upgrade pip
 
 #install tensorflow GPU
 pip install tensorflow-gpu
+
+
+## installing keras
+
+#creating virtual env
+virtualenv kerasPy
+
+#activate the virtual env
+source kerasPy/bin/activate
+
+#upgrade pip to collect latest repository
+pip install --upgrade pip
+
+#install tensorflow GPU
+pip install tensorflow-gpu
+
+#install keras
+pip install keras
