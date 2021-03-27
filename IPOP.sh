030ed15d87ff5c439d182eb8f7755c94b07e6b96
Md Selim <selim.iitdu@gmail.com>
2021-03-27 16:13:53 -0400

    Update IPOP.sh

--- IPOP.sh
+++ IPOP.sh
@@ -11,6 +11,14 @@ jupyter lab --no-browser --port 8802 --ip 10.10.10.210
 pip install torch torchvision cuda=10.2 torch=1.7.0
 pip install tensorflow=2.0.0
 
+#jupytar LAB for Radiomc
+tmux new -s tf_2_1
+virtualenv radiomic_gan_cam
+source radiomic_gan_cam/bin/activate
+jupyter lab --no-browser --port 8802 --ip 10.10.10.210
+pip install torch torchvision cuda=10.2 torch=1.7.0
+pip install tensorflow-gpu==2.1.0
+
 # installing Jupyter notebook (lab)
 pip install jupyterlab
 
